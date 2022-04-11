f9e9829e384305a1be0e196f88701ec58a1dae2c
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-11 20:39:08 -0300

    fix: eslint

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -6,15 +6,13 @@ class SimpleReport():
     def generate(arr):
         today = date.today().strftime("%Y-%m-%d")
         antiga = []
+        proxima = []
+        maior_quantidade = []
         for item in arr:
             if item["data_de_fabricacao"]:
                 antiga.append(item["data_de_fabricacao"])
-        proxima = []
-        for item in arr:
             if item["data_de_validade"] and item["data_de_validade"] > today:
                 proxima.append(item["data_de_validade"])
-        maior_quantidade = []
-        for item in arr:
             if item["nome_da_empresa"]:
                 maior_quantidade.append(item["nome_da_empresa"])
         counter_empresa = Counter(maior_quantidade)
