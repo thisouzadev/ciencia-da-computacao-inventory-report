49e971099f9b300880f2c56f83d036806688699e
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2022-04-11 19:06:23 -0300

    feat: Requisito 1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,26 @@
+from datetime import date
+from collections import Counter
+
+
+class SimpleReport():
+    def generate(arr):
+        today = date.today().strftime("%Y-%m-%d")
+        antiga = []
+        for item in arr:
+            if item["data_de_fabricacao"]:
+                antiga.append(item["data_de_fabricacao"])
+        proxima = []
+        for item in arr:
+            if item["data_de_validade"] and item["data_de_validade"] > today:
+                proxima.append(item["data_de_validade"])
+        maior_quantidade = []
+        for item in arr:
+            if item["nome_da_empresa"]:
+                maior_quantidade.append(item["nome_da_empresa"])
+        counter_empresa = Counter(maior_quantidade)
+        Empresa = "Empresa com maior quantidade de produtos estocados:"
+        return (
+            f"Data de fabricação mais antiga: {min(antiga)}\n"
+            f"Data de validade mais próxima: {min(proxima)}\n"
+            f"{Empresa} {max(counter_empresa)}\n"
+        )
